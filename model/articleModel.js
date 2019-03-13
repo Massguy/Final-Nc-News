f92df5da4a1224210a62d045de548d04065c5669
OEM Configuration (temporary user) <stefin008@hotmail.co.uk>
2019-03-13 07:00:30 -0400

    Passed the first test of having certain properties in the article table including the comment_count

--- model/articleModel.js
+++ model/articleModel.js
@@ -1,3 +1,19 @@
 const connection = require('../db/connection');
 
-exports.getArticles = () => connection.select('article.title', 'article.article_id', 'article.topic', 'article.created_at', 'article.votes').from('article');
+exports.getArticles = () => connection
+  .select(
+    'article.author',
+    'article.title',
+    'article.article_id',
+    'article.topic',
+    'article.created_at',
+    'article.votes',
+  )
+  .from('article')
+  .groupBy('article.author', 'article.title',
+    'article.article_id',
+    'article.topic',
+    'article.created_at',
+    'article.votes')
+  .leftJoin('comment', 'article.article_id', 'comment.comment_id')
+  .count('comment.comment_id as comment_count');
